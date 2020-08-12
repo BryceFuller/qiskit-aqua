473eeb94c1d54998e0ab87c817c60d7f7b430c38
CZ <ouf@zurich.ibm.com>
2020-08-12 15:37:16 +0200

    reshuffle

--- qiskit/aqua/operators/gradients/hessian/hessian.py
+++ qiskit/aqua/operators/gradients/hessian/hessian.py
@@ -35,3 +35,35 @@ from ..gradient_base import GradientBase
 from qiskit.circuit import Parameter, ParameterExpression, ParameterVector
 
 logger = logging.getLogger(__name__)
+
+
+class Hessian(GradientBase):
+    r"""
+    Converter for changing parameterized circuits into operators
+    whose evaluation yields the second-order gradient with respect to the circuit parameters.
+    """
+
+    # pylint: disable=too-many-return-statements
+    def convert(self,
+        operator: OperatorBase = None,
+        param_pairs: Union[Tuple, List] = None,
+        method: str = 'param_shift') -> OperatorBase:
+        r"""
+        Args:
+            operator: The measurement operator we are taking the gradient of
+            state_operator:  The operator corresponding to our state preparation circuit
+            parameters: The parameters we are taking the gradient with respect to
+            method: The method used to compute the gradient. Either 'param_shift' or 'ancilla'
+        Returns:
+            gradient_operator: An operator whose evaluation yeild the Hessian
+        """
+        if isinstance(param_pairs, Tuple):
+            if method == 'param_shift':
+                return self.parameter_shift_grad(self.parameter_shift(operator, param_pairs[0]), param_pairs[1])
+            if method == 'ancilla':
+                return self.ancilla_hessian(param_pairs)
+
+        # Todo fix the ancilla method here
+        return ListOp([self.parameter_shift(self.parameter_shift(operator, pair[0]), pair[1]) for pair in param_pairs])
+
+    # Todo add ancilla hessian here
