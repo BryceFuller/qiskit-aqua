fix probability gradient linear combination

test/aqua/test_grad.py
@@ -24,6 +24,7 @@ from qiskit import BasicAer
 from qiskit.aqua.operators.gradients.gradient.state_gradient_lin_comb import StateGradientLinComb
 from qiskit.aqua.operators.gradients.gradient.prob_gradient_lin_comb import ProbabilityGradientLinComb
 from qiskit.aqua.operators.gradients.hessian.state_hessian_lin_comb import StateHessianLinComb
+from qiskit.aqua.operators.gradients.qfi.qfi import QFI
 
 from qiskit.aqua.operators import X, Z, StateFn, CircuitStateFn
 from qiskit import QuantumCircuit, QuantumRegister
@@ -117,6 +118,7 @@ class TestQuantumFisherInf(QiskitAquaTestCase):
         self.assertTrue(correct_grad)
 
     def test_prob_lin_comb_grad(self):
+
         """Test the ancilla probability gradient
         dp0/da = cos(a)sin(b) / 2
         dp1/da = - cos(a)sin(b) / 2
@@ -136,51 +138,50 @@ class TestQuantumFisherInf(QiskitAquaTestCase):
 
         op = CircuitStateFn(primitive=qc, coeff=1.)
 
-        prob_grad = StateGradientLinComb().convert(operator=op, params=params)
+        prob_grad = ProbabilityGradientLinComb().convert(operator=op, params=params)
         values_dict = [{a: np.pi / 4, b: 0}, {params[0]: np.pi / 4, params[1]: np.pi / 4},
                        {params[0]: np.pi / 2, params[1]: np.pi}]
         correct_values = [[[0, 0], [1/(2*np.sqrt(2)), - 1/(2*np.sqrt(2))]], [[1/4, - 1/4], [1/4, - 1/4]],
                           [[0, 0], [- 1/2, 1/2]]]
         correct_grad = True
         for i, value_dict in enumerate(values_dict):
+            print(prob_grad.assign_parameters(value_dict).eval())
+            # print(prob_grad.assign_parameters(value_dict).eval())
             correct_grad &= np.allclose(prob_grad.assign_parameters(value_dict).eval(), correct_values[i], atol=1e-6)
-
         self.assertTrue(correct_grad)
     #
     # def test_product_rule(self):
     #     # TODO
     #     return
-    #
-    # def test_qfi(self):
-    #     """Test if the quantum fisher information calculation is correct
-    #     QFI = [[1, 0], [0, 1]] - [[0, 0], [0, cos^2(a)]]"""
-    #
-    #     a = Parameter('a')
-    #     b = Parameter('b')
-    #     params = [a, b]
-    #
-    #     q = QuantumRegister(1)
-    #     qc = QuantumCircuit(q)
-    #     qc.h(q)
-    #     qc.rz(params[0], q[0])
-    #     qc.rx(params[1], q[0])
-    #
-    #     # parameterized_gates = []
-    #     # for param, elements in qc._parameter_table.items():
-    #     #     for element in elements:
-    #     #         parameterized_gates.append(element[0])
-    #
-    #     qfi = QFI(circuit=qc, quantum_instance=self.qi)
-    #     values_dict = {params[0]: np.pi / 4, params[1]: 0.1}
-    #     qfi_value=qfi.compute_qfi(params, values_dict)
-    #     correct_qfi = np.allclose(qfi_value, [[1, 0], [0, 0.5]], atol=1e-6)
-    #     values_dict = {params[0]: np.pi, params[1]: 0.1}
-    #     qfi_value = qfi.compute_qfi(params, values_dict)
-    #     correct_qfi &= np.allclose(qfi_value, [[1, 0], [0, 0]], atol=1e-6)
-    #     values_dict = {params[0]: np.pi/2, params[1]: 0.1}
-    #     qfi_value = qfi.compute_qfi(params, values_dict)
-    #     correct_qfi &= np.allclose(qfi_value, [[1, 0], [0, 1]], atol=1e-6)
-    #     self.assertTrue(correct_qfi)
+
+    def test_qfi(self):
+        """Test if the quantum fisher information calculation is correct
+        QFI = [[1, 0], [0, 1]] - [[0, 0], [0, cos^2(a)]]"""
+
+        a = Parameter('a')
+        b = Parameter('b')
+        params = [a, b]
+
+        q = QuantumRegister(1)
+        qc = QuantumCircuit(q)
+        qc.h(q)
+        qc.rz(params[0], q[0])
+        qc.rx(params[1], q[0])
+
+        # parameterized_gates = []
+        # for param, elements in qc._parameter_table.items():
+        #     for element in elements:
+        #         parameterized_gates.append(element[0])
+
+        op = CircuitStateFn(primitive=qc, coeff=1.)
+        qfi = QFI().convert(operator=op, params=params)
+        values_dict = [{params[0]: np.pi / 4, params[1]: 0.1}, {params[0]: np.pi, params[1]: 0.1},
+                       {params[0]: np.pi/2, params[1]: 0.1}]
+        correct_values = [[[1, 0], [0, 0.5]], [[1, 0], [0, 0]],  [[1, 0], [0, 1]]]
+        correct_qfi = True
+        for i, value_dict in enumerate(values_dict):
+            correct_qfi &= np.allclose(qfi.assign_parameters(value_dict).eval(), correct_values[i], atol=1e-6)
+        self.assertTrue(correct_qfi)
 
 
 if __name__ == '__main__':