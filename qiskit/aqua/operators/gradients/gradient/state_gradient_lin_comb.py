a7f4751ba1759e3f6e72bea87c7a166aae6221e2
Cryoris <jules.gacon@googlemail.com>
2020-08-21 11:51:50 +0200

    fix most lints

--- qiskit/aqua/operators/gradients/gradient/state_gradient_lin_comb.py
+++ qiskit/aqua/operators/gradients/gradient/state_gradient_lin_comb.py
@@ -12,64 +12,50 @@
 # copyright notice, and modified files need to carry a notice indicating
 # that they have been altered from the originals.
 
-""" StateGradient Class """
+"""The module to compute the state gradient with the linear combination method."""
 
-from typing import Optional, Callable, Union, List, Dict
-import logging
-from functools import partial, reduce
-import numpy as np
+from typing import Optional, Union, List
 from copy import deepcopy
+import numpy as np
 
-from qiskit.quantum_info import Pauli
-from qiskit import QuantumCircuit, QuantumRegister
-from qiskit.circuit import Instruction, Gate
+from qiskit.circuit import QuantumCircuit, QuantumRegister, Parameter, ParameterVector
+from qiskit.circuit.library import ZGate, SGate, SdgGate, HGate
 
-from qiskit.aqua.operators import OperatorBase, ListOp, CircuitOp, PauliOp
+from qiskit.aqua.operators import OperatorBase, ListOp, CircuitOp
 from qiskit.aqua.operators.primitive_ops.primitive_op import PrimitiveOp
 from qiskit.aqua.operators.converters import DictToCircuitSum
 from qiskit.aqua.operators.state_fns import StateFn, CircuitStateFn, DictStateFn, VectorStateFn
-from qiskit.aqua.operators.operator_globals import H, S, I, Z
-from qiskit.aqua.operators.expectations import PauliExpectation
+from qiskit.aqua.operators.operator_globals import Z
 from ..gradient_base import GradientBase
-
-from qiskit.circuit import Parameter, ParameterExpression, ParameterVector
-
-from qiskit.circuit.library.standard_gates import RXGate, CRXGate, RYGate, CRYGate, RZGate, CRZGate, CXGate, CYGate, \
-    CZGate,U1Gate, U2Gate, U3Gate, RXXGate, RYYGate, RZZGate, RZXGate, CU1Gate, MCU1Gate, CU3Gate, IGate, HGate, \
-    XGate, SdgGate, SGate, ZGate
-
-logger = logging.getLogger(__name__)
 
 
 class StateGradientLinComb(GradientBase):
-    r"""
-    We are interested in computing:
-    d⟨ψ(ω)|O(θ)|ψ(ω)〉/ dω  for ω in params
-    """
+    """Compute the state gradient d⟨ψ(ω)|O(θ)|ψ(ω)〉/ dω using the linear combination method."""
 
     def convert(self,
-                operator: OperatorBase = None,
-                params: Union[Parameter, ParameterVector, List] = None) -> OperatorBase:
-        r"""
-        Args
+                operator: OperatorBase,
+                params: Optional[Union[Parameter, ParameterVector, List[Parameter]]] = None
+                ) -> OperatorBase:
+        """
+        Args:
             operator: The operator we are taking the gradient of: ⟨ψ(ω)|O(θ)|ψ(ω)〉
             params: The parameters we are taking the gradient wrt: ω
-        Returns
+
+        Returns:
             ListOp where the ith operator corresponds to the gradient wrt params[i]
         """
         self._params = params
-
         return self._prepare_operator(operator)
 
-    # TODO remove here. Taverse should happen in gradient
+    # TODO remove here. Traverse should happen in gradient
     def _prepare_operator(self, operator):
         if isinstance(operator, ListOp):
             return operator.traverse(self._prepare_operator)
         elif isinstance(operator, StateFn):
-            if operator.is_measurement == True:
+            if operator.is_measurement:
                 return operator.traverse(self._prepare_operator)
         elif isinstance(operator, PrimitiveOp):
-            return (Z ^ operator)
+            return Z ^ operator
         if isinstance(operator, (QuantumCircuit, CircuitStateFn, CircuitOp)):
             # TODO avoid duplicate transformations
             operator = self._grad_states(operator, self._params)
@@ -77,34 +63,40 @@ class StateGradientLinComb(GradientBase):
 
     def _grad_states(self,
                      op: OperatorBase,
-                     target_params: Union[Parameter, ParameterVector, List] = None) -> ListOp:
-                             # state_qc: QuantumCircuit,
-                             # gates_to_parameters: Dict[Parameter, List[Gate]],
-                             # grad_coeffs: Dict[Parameter, List[List[complex]]],
-                             # grad_gates: Dict[Parameter, List[List[Instruction]]])
+                     target_params: Optional[Union[Parameter, ParameterVector, List]] = None
+                     ) -> ListOp:
         """Generate the gradient states.
 
         Args:
             op: The operator representing the quantum state for which we compute the gradient.
             target_params: The parameters we are taking the gradient wrt: ω
-            state_qc: The quantum circuit representing the state for which we compute the gradient.
-            gates_to_parameters: The dictionary of parameters and gates with respect to which the quantum Fisher
-            Information is computed.
-            grad_coeffs: The values needed to compute the gradient for the parameterized gates.
-                    For each parameter, the dict holds a list of all coeffs for all gates which are parameterized by
-                    the parameter. {param:[[coeffs0],...]}
-            grad_gates: The gates needed to compute the gradient for the parameterized gates.
-                    For each parameter, the dict holds a list of all gates to insert for all gates which are
-                    parameterized by the parameter. {param:[[gates_to_insert0],...]}
 
         Returns:
-            ListOp of StateFns as quantum circuits which are the states w.r.t. which we compute the gradient.
-            If a parameter appears multiple times, one circuit is created per parameterized gates to compute
-            the product rule.
+            ListOp of StateFns as quantum circuits which are the states w.r.t. which we compute the
+            gradient. If a parameter appears multiple times, one circuit is created per
+            parameterized gates to compute the product rule.
 
         Raises:
             AquaError: If one of the circuits could not be constructed.
+            TypeError: If the operators is of unsupported type.
         """
+        # TODO what's with these arguments?
+        # state_qc: QuantumCircuit,
+        # gates_to_parameters: Dict[Parameter, List[Gate]],
+        # grad_coeffs: Dict[Parameter, List[List[complex]]],
+        # grad_gates: Dict[Parameter, List[List[Instruction]]])
+        # state_qc: The quantum circuit representing the state for which we compute the gradient.
+        # gates_to_parameters: The dictionary of parameters and gates with respect to which the
+        #   quantum Fisher
+        # Information is computed.
+        # grad_coeffs: The values needed to compute the gradient for the parameterized gates.
+        #      For each parameter, the dict holds a list of all coeffs for all gates which are
+        #      parameterized by
+        #      the parameter. {param:[[coeffs0],...]}
+        # grad_gates: The gates needed to compute the gradient for the parameterized gates.
+        #      For each parameter, the dict holds a list of all gates to insert for all gates
+        #      which are parameterized by the parameter. {param:[[gates_to_insert0],...]}
+
         # Dictionary with the information which parameter is used in which gate
         gates_to_parameters = {}
         # Dictionary which relates the coefficients needed for the QFI for every parameter
@@ -114,10 +106,10 @@ class StateGradientLinComb(GradientBase):
         # Loop throuh the parameters in the circuit
         params = []
 
-        if isinstance(op, CircuitStateFn) or isinstance(op, CircuitOp):
+        if isinstance(op, (CircuitStateFn, CircuitOp)):
             pass
-        elif isinstance(op, DictStateFn) or isinstance(op, VectorStateFn):
-            op = DictToCircuitSum.convert(op) #Todo inplace
+        elif isinstance(op, (DictStateFn, VectorStateFn)):
+            op = DictToCircuitSum().convert(op)  # TODO inplace
         else:
             raise TypeError('Ancilla gradients only support operators whose states are either '
                             'CircuitStateFn, DictStateFn, or VectorStateFn.')
@@ -132,7 +124,8 @@ class StateGradientLinComb(GradientBase):
             grad_coeffs[param] = []
             grad_gates[param] = []
             for element in elements:
-                # get the coefficients and controlled gates (raises an error if the parameterized gate is not supported)
+                # get the coefficients and controlled gates (raises an error if the parameterized
+                # gate is not supported)
                 coeffs_gates = self.gate_gradient_dict(element[0])
                 gates_to_parameters[param].append(element[0])
                 for c_g in coeffs_gates:
@@ -145,39 +138,44 @@ class StateGradientLinComb(GradientBase):
         additional_qubits = ([work_q], [])
         # create a copy of the original state with an additional work_q register
         # Get the states needed to compute the gradient
-        for i in range(len(params)):  # loop over parameters
+        for param in params:  # loop over parameters
             # construct the states
-            for m, gates_to_insert_i in enumerate(grad_gates[params[i]]):
+            for m, gates_to_insert_i in enumerate(grad_gates[param]):
                 for k, gate_to_insert_i in enumerate(gates_to_insert_i):
                     grad_state = QuantumCircuit(*state_qc.qregs, qr_work)
-                    grad_state.data = state_qc.data
+                    grad_state.compose(state_qc, inplace=True)
+
                     # apply Hadamard on work_q
-                    self.insert_gate(grad_state, gates_to_parameters[params[0]][0], HGate(), qubits=[work_q])
+                    # TODO can this not just be grad_state.h(qr_work) ?
+                    self.insert_gate(
+                        grad_state, gates_to_parameters[params[0]][0], HGate(), qubits=[work_q]
+                    )
+
                     # Fix work_q phase
-                    coeff_i = grad_coeffs[params[i]][m][k]
+                    coeff_i = grad_coeffs[param][m][k]
                     sign = np.sign(coeff_i)
-                    complex = np.iscomplex(coeff_i)
+                    is_complex = np.iscomplex(coeff_i)
                     if sign == -1:
-                        if complex:
-                            self.insert_gate(grad_state, gates_to_parameters[params[0]][0], SdgGate(),
-                                        qubits=[work_q])
+                        if is_complex:
+                            self.insert_gate(grad_state, gates_to_parameters[params[0]][0],
+                                             SdgGate(), qubits=[work_q])
                         else:
-                            self.insert_gate(grad_state, gates_to_parameters[params[0]][0], ZGate(),
-                                        qubits=[work_q])
+                            self.insert_gate(grad_state, gates_to_parameters[params[0]][0],
+                                             ZGate(), qubits=[work_q])
                     else:
-                        if complex:
-                            self.insert_gate(grad_state, gates_to_parameters[params[0]][0], SGate(),
-                                        qubits=[work_q])
+                        if is_complex:
+                            self.insert_gate(grad_state, gates_to_parameters[params[0]][0],
+                                             SGate(), qubits=[work_q])
                     # Insert controlled, intercepting gate - controlled by |0>
-                    self.insert_gate(grad_state, gates_to_parameters[params[i]][m],
-                                gate_to_insert_i,
-                                additional_qubits=additional_qubits)
+                    self.insert_gate(grad_state, gates_to_parameters[param][m],
+                                     gate_to_insert_i,
+                                     additional_qubits=additional_qubits)
                     grad_state.h(work_q)
                     if m == 0 and k == 0:
                         state = np.sqrt(np.abs(coeff_i)) * CircuitStateFn(grad_state)
                     else:
                         state += np.sqrt(np.abs(coeff_i)) * CircuitStateFn(grad_state)
+
             states += [state]
             #  TODO check that all properties of op are carried over but I think so
         return ListOp(states) * op.coeff
-
--- qiskit/aqua/operators/gradients/gradient/state_gradient_param_shift.py
+++ qiskit/aqua/operators/gradients/gradient/state_gradient_param_shift.py
@@ -12,60 +12,41 @@
 # copyright notice, and modified files need to carry a notice indicating
 # that they have been altered from the originals.
 
-""" StateGradient Class """
+"""The module to compute the state gradient with the parameter shift rule."""
 
-from typing import Optional, Callable, Union, List, Dict
-import logging
-from functools import partial, reduce
-import numpy as np
+from typing import Optional, Union, List
 
-from qiskit.quantum_info import Pauli
-from qiskit import QuantumCircuit, QuantumRegister
-from qiskit.circuit import Instruction, Gate
-
-from qiskit.aqua.operators import OperatorBase, ListOp
-from qiskit.aqua.operators.primitive_ops.primitive_op import PrimitiveOp
-from qiskit.aqua.operators.converters import DictToCircuitSum
-from qiskit.aqua.operators.state_fns import StateFn, CircuitStateFn, DictStateFn, VectorStateFn
-from qiskit.aqua.operators.operator_globals import H, S, I, Z
-from qiskit.aqua.operators.expectations import PauliExpectation
+from qiskit.circuit import Parameter, ParameterVector
+from qiskit.aqua.operators import OperatorBase
 from ..gradient_base import GradientBase
-from qiskit.circuit import Parameter, ParameterExpression, ParameterVector
-
-from qiskit.circuit.library.standard_gates import RXGate, CRXGate, RYGate, CRYGate, RZGate, CRZGate, CXGate, CYGate, CZGate,\
-    U1Gate, U2Gate, U3Gate, RXXGate, RYYGate, RZZGate, RZXGate, CU1Gate, MCU1Gate, CU3Gate, IGate, HGate, XGate, \
-    SdgGate, SGate, ZGate
-
-logger = logging.getLogger(__name__)
 
 
 class StateGradientParamShift(GradientBase):
-    r"""
-    We are interested in computing:
-    d⟨ψ(ω)|O(θ)|ψ(ω)〉/ dω  for ω in params
-    """
+    """Compute the gradient d⟨ψ(ω)|O(θ)|ψ(ω)〉/ dω with the parameter shift method."""
 
     def convert(self,
-                operator: OperatorBase = None,
-                params: Union[Parameter, ParameterVector, List] = None,
-                analytic: bool = True) -> OperatorBase:
-        r"""
-        Args
-            state_operator:The operator corresponding to our quantum state we are taking the gradient of: |ψ(ω)〉
-            observable_operator: The measurement operator we are taking the gradient of: O(θ)
+                operator: OperatorBase,
+                params: Optional[Union[Parameter, ParameterVector, List[Parameter]]] = None
+                ) -> OperatorBase:
+        """
+        Args:
+            operator: The operator corresponding to our quantum state we are taking the
+                gradient of: |ψ(ω)〉
             params: The parameters we are taking the gradient wrt: ω
-            analytic: If true compute an analytic gradient, else compute a finite difference approximations
-        Returns
+
+        Returns:
             ListOp where the ith operator corresponds to the gradient wrt params[i]
         """
-
         # TODO add finite difference
+        # analytic: bool = True
+        # analytic: If True compute an analytic gradient, else compute a finite difference
+        #     approximations
 
-        # TODO Look through state and decompose gates which cannot be evaluated with the parameter shift rule
-        # This seems like it could be it's own converter??
+        # TODO Look through state and decompose gates which cannot be evaluated with the parameter
+        # shift rule. This seems like it could be it's own converter??
         # decomposed_state = self.decompose_to_two_unique_eigenval(state_operator, params)
+
         # TODO Note to above --> all are pi/2
         # parameter_shift will return a ListOp of the same size as params
         # one SummedOp per parameter.
         return self.parameter_shift(operator, params)
-
