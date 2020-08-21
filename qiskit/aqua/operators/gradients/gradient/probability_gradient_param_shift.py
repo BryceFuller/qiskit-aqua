929b7be4e9ebf1c7d6fdd80a93c9041b61aeecdc
Cryoris <jules.gacon@googlemail.com>
2020-08-21 12:03:21 +0200

    fix most lint

--- qiskit/aqua/operators/gradients/gradient/probability_gradient_param_shift.py
+++ qiskit/aqua/operators/gradients/gradient/probability_gradient_param_shift.py
@@ -12,64 +12,60 @@
 # copyright notice, and modified files need to carry a notice indicating
 # that they have been altered from the originals.
 
-""" ProbabilityGradient Class """
+"""The module to compute the probability gradients with the parameter shift method."""
 
-from typing import Optional, Callable, Union, List, Dict
-import logging
-from functools import partial, reduce
-import numpy as np
+from typing import Optional, Union, List, Dict
+from functools import partial
 
-from qiskit.quantum_info import Pauli
-from qiskit import Aer, QuantumCircuit
+from qiskit import Aer
 from qiskit.providers import BaseBackend
 from qiskit.aqua import QuantumInstance, AquaError
-from qiskit.circuit import Parameter, ParameterExpression, ParameterVector
+from qiskit.circuit import Parameter, ParameterVector
 
 from qiskit.aqua.operators.operator_base import OperatorBase
-from qiskit.aqua.operators.primitive_ops.primitive_op import PrimitiveOp
-from qiskit.aqua.operators.primitive_ops.pauli_op import PauliOp
-from qiskit.aqua.operators.primitive_ops.circuit_op import CircuitOp
 from qiskit.aqua.operators.list_ops.list_op import ListOp
-from qiskit.aqua.operators.list_ops.composed_op import ComposedOp
 from qiskit.aqua.operators.state_fns.state_fn import StateFn
-from qiskit.aqua.operators.operator_globals import H, S, I
 from qiskit.aqua.operators.converters.circuit_sampler import CircuitSampler
 from .state_gradient_param_shift import StateGradientParamShift
 
-logger = logging.getLogger(__name__)
-
 
 class ProbabilityGradientParamShift(StateGradientParamShift):
-    r"""
-    Special Case of the StateGradient where the gradient_operator is the identity
-    (Actually there are some more nuanced differences on how this is computed in practice!)
+    """Special Case of the StateGradient where the gradient_operator is the identity.
 
-    We are interested in computing:
-    d⟨ψ(ω)|ψ(ω)〉/ dω for ω in params
+    Actually there are some more nuanced differences on how this is computed in practice!
     """
 
-    # pylint: disable=too-many-return-statements
     def convert(self,
-                operator: OperatorBase = None,
-                params: Union[Parameter, ParameterVector, List] = None,
-                param_bindings = None,
-                backend = None,
+                operator: OperatorBase,
+                params: Union[Parameter, ParameterVector, List[Parameter]] = None,
+                param_bindings: Optional[Dict[Parameter, float]] = None,
+                backend: Optional[QuantumInstance, BaseBackend] = None,
                 analytic: bool = True) -> OperatorBase:
         r"""
         Args
-            state_operator: |ψ(ω)〉, The operator corresponding to our quantum state we are taking the gradient of ()
+            operator: The operator corresponding to our quantum state we are taking the
+                gradient of: prepares |ψ(ω)〉.
             params: The parameters we are taking the gradient with respect to
-            analytic: If true compute an analytic gradient, else compute a finite difference approximations
+            param_bindings: TODO
+            backend: The backend for the circuit sampler. If None, Aer's QASM backend is chosen
+                by default with 1024 shots.
+            analytic: If True compute an analytic gradient, else compute a finite difference
+                approximations.
         """
         # TODO add finite differences
+        if analytic is False:
+            raise NotImplementedError('Finite difference scheme not implemented yet.')
+
         # TODO backend and param bindings?
         if operator.is_measurement:
-            raise AquaError('Probability gradients are computed with respect to states instead of expectation values.'
-                            'Please remove the measurement operator.')
+            raise AquaError('Probability gradients are computed with respect to states instead of '
+                            'expectation values. Please remove the measurement operator.')
 
         operator = super().convert(operator, params)
 
         if backend is None:
+            # TODO don't rely on Aer, user might not have installed this, rather just use eval
+            # and not the circuit sampler
             backend = QuantumInstance(backend=Aer.get_backend('qasm_simulator'),
                                       shots=1024)
 
@@ -78,9 +74,9 @@ class ProbabilityGradientParamShift(StateGradientParamShift):
 
         # The CircuitStateFn's need to be wrapped so that the coefficient
         # is stored elsewhere during circuit sampling
-        wrapped_op = self.wrap_circuit_statefn(operator)
+        wrapped_op = self._wrap_circuit_statefn(operator)
         sampled_op = sampler.convert(wrapped_op.bind_parameters(param_bindings))
-        grad_op = self.ravel(sampled_op).reduce()
+        grad_op = self._ravel(sampled_op).reduce()
 
         if isinstance(grad_op, ListOp):
             gradients = [grad.primitive for grad in grad_op.reduce()]
@@ -88,19 +84,17 @@ class ProbabilityGradientParamShift(StateGradientParamShift):
             gradients = [grad_op.primitive]
         return gradients
 
-    def wrap_circuit_statefn(self, operator):
+    def _wrap_circuit_statefn(self, operator):
         if isinstance(operator, StateFn):
             return ListOp([operator / operator.coeff], coeff=operator.coeff)
         elif isinstance(operator, ListOp):
-            return operator.traverse(partial(self.wrap_circuit_statefn))
-        else:
-            return operator
+            return operator.traverse(partial(self._wrap_circuit_statefn))
+        return operator
 
-    def ravel(self, operator):
+    def _ravel(self, operator):
         if isinstance(operator, ListOp):
             if len(operator.oplist) == 1:
                 return operator.oplist[0] * operator.coeff
             else:
-                return operator.traverse(partial(self.ravel))
-        else:
-            return operator
+                return operator.traverse(partial(self._ravel))
+        return operator
