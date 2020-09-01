Fix normalization issue in the LCU Hessian

qiskit/aqua/operators/gradients/hessian/hessian_lin_comb.py
@@ -16,16 +16,17 @@
 from collections.abc import Iterable
 from typing import Optional, Union, List
 from copy import deepcopy
+from functools import partial
 import numpy as np
 
 from qiskit.circuit import QuantumCircuit, QuantumRegister, Parameter, ParameterVector, ParameterExpression
 from qiskit.circuit.library import HGate, SGate, SdgGate, ZGate
+from qiskit.quantum_info import partial_trace
 
-from qiskit.aqua.operators import OperatorBase, ListOp, CircuitOp
+from qiskit.aqua.operators import OperatorBase, ListOp, CircuitOp, ComposedOp
 from qiskit.aqua.operators.primitive_ops.primitive_op import PrimitiveOp
-from qiskit.aqua.operators.converters import DictToCircuitSum
 from qiskit.aqua.operators.state_fns import StateFn, CircuitStateFn, DictStateFn, VectorStateFn
-from qiskit.aqua.operators.operator_globals import I, Z
+from qiskit.aqua.operators.operator_globals import I, Z, One, Zero
 
 from ..gradient_base import GradientBase
 
@@ -47,29 +48,48 @@ class HessianLinComb(GradientBase):
             ListOp[ListOp] where the operator at position k,l corresponds to
             d^2⟨ψ(ω)|O(θ)|ψ(ω)〉/ dω_kdω_l
         """
-        self._params = params
-        return self._prepare_operator(operator)
+        return self._prepare_operator(operator, params)
 
-    def _prepare_operator(self, operator):
-        if isinstance(operator, ListOp):
-            return operator.traverse(self._prepare_operator)
+    def _prepare_operator(self, operator, params):
+        if isinstance(operator, ComposedOp):
+            if not isinstance(operator[0], StateFn) or not operator[0]._is_measurement:
+                raise ValueError("The given operator does not correspond to an expectation value")
+            if not isinstance(operator[-1], StateFn) or operator[-1]._is_measurement:
+                raise ValueError("The given operator does not correspond to an expectation value")
+            if operator[0].is_measurement:
+                if len(operator.oplist) == 2:
+                    state_op = operator[1]
+                    return self._hessian_states(state_op, meas_op=(~StateFn(Z) ^ operator[0]),
+                                             target_params=params)
+                else:
+                    state_op = deepcopy(operator)
+                    state_op.oplist.pop(0)
+                    return state_op.traverse(partial(self._hessian_states, meas_op=(~StateFn(Z) ^ operator[0]),
+                                                     target_params=params))
+
+            else:
+                return operator.traverse(partial(self._prepare_operator, params=params))
+        elif isinstance(operator, ListOp):
+            return operator.traverse(partial(self._prepare_operator, params=params))
         elif isinstance(operator, StateFn):
             if operator.is_measurement:
-                return operator.traverse(self._prepare_operator)
+                self._operator_has_measurement = True
+                return operator.traverse(partial(self._prepare_operator, params=params))
         elif isinstance(operator, PrimitiveOp):
-            # The division by 4 is necessary to compensate for normalization of summed StateFns
-            return Z ^ I ^ operator / 4
-        if isinstance(operator, (QuantumCircuit, CircuitStateFn, CircuitOp)):
-            # operator.primitive.add_register(QuantumRegister(1, name="ancilla"))
-            operator = self._hessian_states(operator, self._params)
+            return operator
+        elif isinstance(operator, (CircuitStateFn, CircuitOp)):
+            return self._hessian_states(operator, target_params=params)
         return operator
 
-    def _hessian_states(self, op: OperatorBase,
-                        target_params: Union[Parameter, ParameterVector, List] = None) -> ListOp:
+    def _hessian_states(self,
+                        state_op: OperatorBase,
+                        meas_op: Optional(OperatorBase) = None,
+                        target_params: Optional(Union(Parameter, ParameterVector, List)) = None) -> ListOp:
         """Generate the operators whose evaluation leads to the full QFI.
 
         Args:
-            op: The operator representing the quantum state for which we compute the hessian.
+            state_op: The operator representing the quantum state for which we compute the hessian.
+            meas_op: The operator representing the observable for which we compute the gradient.
             target_params: The parameters we are computing the hessian wrt: ω
 
         Returns:
@@ -88,7 +108,7 @@ class HessianLinComb(GradientBase):
         # Dictionary which relates the gates needed for the hessian for every parameter
         hessian_gates = {}
         # Get the quantum circuit corresponding to the state operator
-        state_qc = deepcopy(op.primitive)
+        state_qc = deepcopy(state_op.primitive)
         if not isinstance(target_params, Iterable):
             target_params = [target_params]
         for param in target_params:
@@ -202,34 +222,96 @@ class HessianLinComb(GradientBase):
                                 hessian_circuit.cz(work_q1, work_q0)
                                 hessian_circuit.h(work_q1)
 
-                                term = op.coeff * np.sqrt(np.abs(coeff_a) * np.abs(coeff_b)) * \
-                                    CircuitStateFn(hessian_circuit)
+                                term = state_op.coeff * np.sqrt(np.abs(coeff_a) * np.abs(coeff_b)) * \
+                                       CircuitStateFn(hessian_circuit)
 
                                 # Chain Rule Parameter Expression
                                 gate_param = gates_to_parameters[param_a][m].params[j]
-                                if gate_param == param_a:
-                                    pass
-                                else:
-                                    if isinstance(gate_param, ParameterExpression):
-                                        import sympy as sy
-                                        expr_grad = self.parameter_expression_grad(gate_param, param_a)
-                                        # Square root needed bc the coefficients are squared in the expectation value
-                                        expr_grad._symbol_expr = sy.sqrt(expr_grad._symbol_expr)
-                                        term *= expr_grad
+                                if meas_op:
+                                    if gate_param == param_a:
+                                        term = meas_op @ term
                                     else:
-                                        term *= 0
+                                        if isinstance(gate_param, ParameterExpression):
+                                            import sympy as sy
+                                            expr_grad = self.parameter_expression_grad(gate_param, param_a)
+                                            term = (expr_grad * meas_op) @ term
+                                        else:
+                                            term = ~StateFn(One) @ Zero
+                                else:
+                                    def combo_fn(x):
+                                        # TODO parameter expression
+                                        x = x.primitive
+                                        # Generate the operator which computes the linear combination
+                                        lin_comb_op = (I ^ state_op.num_qubits) ^ Z
+                                        lin_comb_op = lin_comb_op.to_matrix()
+                                        # Compute a partial trace over the working qubit needed to compute the linear combination
+                                        if isinstance(x, list) or isinstance(x, np.ndarray):
+                                            # TODO check if output is prob or sv - in case of prob get rid of np.dot
+                                            return [np.diag(
+                                                partial_trace(lin_comb_op.dot(np.outer(item, np.conj(item))), [0]).data)
+                                                    for item in x]
+                                        else:
+                                            # TODO check if output is prob or sv - in case of prob get rid of np.dot
+                                            return np.diag(
+                                                partial_trace(lin_comb_op.dot(np.outer(x, np.conj(x))), [0]).data)
+
+                                    if gate_param == param_a:
+                                        pass
+                                    else:
+                                        if isinstance(gate_param, ParameterExpression):
+                                            import sympy as sy
+                                            expr_grad = self.parameter_expression_grad(gate_param, param_a)
+                                            # Square root needed bc the coefficients are squared in the expectation value
+                                            # TODO enable complex parameter expressions
+                                            expr_grad._symbol_expr = sy.sqrt(expr_grad._symbol_expr)
+                                            term *= expr_grad
+                                        else:
+                                            term = ~StateFn(One) @ Zero
+                                    term = ListOp(term, combo_fn=combo_fn)
+
                                 gate_param = gates_to_parameters[param_b][m].params[n]
-                                if gate_param == param_b:
-                                    pass
-                                else:
-                                    if isinstance(gate_param, ParameterExpression):
-                                        import sympy as sy
-                                        expr_grad = self.parameter_expression_grad(gate_param, param_b)
-                                        # Square root needed bc the coefficients are squared in the expectation value
-                                        expr_grad._symbol_expr = sy.sqrt(expr_grad._symbol_expr)
-                                        term *= expr_grad
+                                
+                                if meas_op:
+                                    if gate_param == param_b:
+                                        term = meas_op @ term
                                     else:
-                                        term *= 0
+                                        if isinstance(gate_param, ParameterExpression):
+                                            import sympy as sy
+                                            expr_grad = self.parameter_expression_grad(gate_param, param_b)
+                                            term = (expr_grad * meas_op) @ term
+                                        else:
+                                            term = ~StateFn(One) @ Zero
+                                else:
+                                    def combo_fn(x):
+                                        # TODO parameter expression
+                                        x = x.primitive
+                                        # Generate the operator which computes the linear combination
+                                        lin_comb_op = (I ^ state_op.num_qubits) ^ Z
+                                        lin_comb_op = lin_comb_op.to_matrix()
+                                        # Compute a partial trace over the working qubit needed to compute the linear combination
+                                        if isinstance(x, list) or isinstance(x, np.ndarray):
+                                            # TODO check if output is prob or sv - in case of prob get rid of np.dot
+                                            return [np.diag(
+                                                partial_trace(lin_comb_op.dot(np.outer(item, np.conj(item))), [0]).data)
+                                                    for item in x]
+                                        else:
+                                            # TODO check if output is prob or sv - in case of prob get rid of np.dot
+                                            return np.diag(
+                                                partial_trace(lin_comb_op.dot(np.outer(x, np.conj(x))), [0]).data)
+
+                                    if gate_param == param_b:
+                                        pass
+                                    else:
+                                        if isinstance(gate_param, ParameterExpression):
+                                            import sympy as sy
+                                            expr_grad = self.parameter_expression_grad(gate_param, param_b)
+                                            # Square root needed bc the coefficients are squared in the expectation value
+                                            # TODO enable complex parameter expressions
+                                            expr_grad._symbol_expr = sy.sqrt(expr_grad._symbol_expr)
+                                            term *= expr_grad
+                                        else:
+                                            term = ~StateFn(One) @ Zero
+                                    term = ListOp(term, combo_fn=combo_fn)
 
                                 if i == 0 and j == 0 and m == 0 and n == 0:
                                     hessian_op = term